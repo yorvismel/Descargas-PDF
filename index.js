b8ff74c860fbd8ac5502607869544c89ff7a905f
yorvismel <112412149+yorvismel@users.noreply.github.com>
2023-06-20 19:33:31 -0500

    modificacion js

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-let identificacion = '';
-const clientes = {
-    15724410: ['15724410', 'Yorvis Alfredo Melendez', '30000 pesos', 'banco popular', -5000],
-    859715: ['859715', 'Juanito Gonzalez', '20000 pesos', 'Banco unido', -10000],
-    6663979: ['6663979', 'Jorge Ivan Cano Macias', '1000000 pesos', 'Bancolombia', -500000]
-};
+document.addEventListener('DOMContentLoaded', function() {
+    let identificacion = '';
+    const clientes = {
+        12345: ['12345', 'Yorvis Alfredo Melendez', '30000 pesos', 'banco popular', -5000],
+        859715: ['859715', 'Juanito Gonzalez', '20000 pesos', 'Banco unido', -10000],
+        678910: ['678910', 'Jorge Ivan Cano Macias', '1000000 pesos', 'Bancolombia', -500000]
+    };
 
-while (identificacion === '') {
-    identificacion = prompt('Introduzca su número de identidad');
-}
+    while (identificacion === '') {
+        identificacion = prompt('Introduzca su número de identidad');
+    }
 
-if (clientes.hasOwnProperty(identificacion)) {
-    const cliente = clientes[identificacion];
-    const informacion = document.getElementById('informacion');
-    informacion.textContent = `Número de cédula: ${cliente[0]}\nNombre: ${cliente[1]}\nSaldo a su favor: ${cliente[2]}\nBanco: ${cliente[3]}\nDeuda: ${cliente[4]}`;
+    if (clientes.hasOwnProperty(identificacion)) {
+        const cliente = clientes[identificacion];
+        const informacion = document.getElementById('informacion');
+        informacion.textContent = `Número de cédula: ${cliente[0]}\nNombre: ${cliente[1]}\nSaldo a su favor: ${cliente[2]}\nBanco: ${cliente[3]}\nDeuda: ${cliente[4]}`;
 
-    // Manejar el evento de clic en el botón de descarga
-    const btnDescargarPDF = document.getElementById('btnDescargarPDF');
-    btnDescargarPDF.addEventListener('click', function() {
-        // Crear un nuevo documento PDF
-        const doc = new jsPDF();
-        // Agregar el contenido al PDF
-        
-        doc.text(informacion.textContent, 10, 10);
-        // Guardar el archivo PDF con el nombre deseado
-        doc.save('informacion_cliente.pdf');
-    });
-}
+        // Manejar el evento de clic en el botón de descarga
+        const btnDescargarPDF = document.getElementById('btnDescargarPDF');
+        btnDescargarPDF.addEventListener('click', function() {
+            // Crear un nuevo documento PDF
+            const doc = new jspdf.jsPDF();
 
-const elemento = document.getElementById('saludo');
-elemento.textContent = `Bienvenido(a) ${identificacion} a nuestra sucursal`;
+            // Agregar el contenido al PDF
+            doc.text(informacion.textContent, 10, 10);
+
+            // Guardar el archivo PDF con el nombre del cliente
+            const nombreArchivo = `Factura de: ${cliente[1]}.pdf`;
+            doc.save(nombreArchivo);
+        });
+    }
+
+    const elemento = document.getElementById('saludo');
+    elemento.textContent = `Bienvenido(a) ${identificacion} a nuestra sucursal`;
+});
